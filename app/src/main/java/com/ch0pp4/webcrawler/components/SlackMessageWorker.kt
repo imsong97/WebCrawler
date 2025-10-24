33449ec1529d816f76410d4992312a6b06fda105
SONG <imsong1997@gmail.com>
2025-10-24 18:37:44 +0900

    CoroutineWorker 작업중

--- app/src/main/java/com/ch0pp4/webcrawler/components/SlackMessageWorker.kt
+++ app/src/main/java/com/ch0pp4/webcrawler/components/SlackMessageWorker.kt
@@ -1,75 +1,14 @@
 package com.ch0pp4.webcrawler.components
 
 import android.content.Context
-import android.webkit.WebView
-import androidx.work.RxWorker
+import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
-import com.ch0pp4.slack.SlackRepository
-import com.ch0pp4.slack.local.SlackPreferenceWrapper
-import com.ch0pp4.webcrawler.crawler.WebCrawlerHelper
-import io.reactivex.Single
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
-import java.util.Calendar
-import kotlin.onSuccess
 
 class SlackMessageWorker (
     private val context: Context,
     private val workerParameters: WorkerParameters
-) : RxWorker(context, workerParameters) {
-
-    override fun createWork(): Single<Result> =
-        if (blockCrawling()) {
-            Single.just(Result.success())
-        } else {
-            Single.create { emitter ->
-                try {
-                    val listener = object : WebCrawlerHelper.CrawlerCallback {
-                        override fun getTagId(id: String) {
-                            emitter.onSuccess(id)
-                        }
-                    }
-                    WebCrawlerHelper(listener, WebView(context)).initDameWeb()
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                    emitter.onError(Throwable("Crawling Error"))
-                }
-            }.subscribeOn(AndroidSchedulers.mainThread())
-            .observeOn(Schedulers.io())
-            .map {
-                val pref = SlackPreferenceWrapper(context)
-                val isNew = pref.getIsNewFlag()
-                val id = pref.getExistId().ifEmpty { "value is empty" }
-
-                if (isNew) {
-                    "‼️New product is Detected‼️\n++new id : $id++"
-                } else {
-                    "++same id : $id++"
-                }
-            }
-            .onErrorReturn {
-                it.message
-            }
-            .flatMap {
-                SlackRepository.instance?.sendSlackMessage(it) ?: Single.just(false)
-            }
-            .map {
-                if (it) {
-                    Result.success()
-                } else {
-                    Result.failure()
-                }
-            }
-        }
-
-    /**
-     * 0~7시 대 타임 및 일요일 크롤링 block
-     * */
-    private fun blockCrawling(): Boolean {
-        val calendar = Calendar.getInstance()
-        val hour = calendar.get(Calendar.HOUR_OF_DAY)
-        val day = calendar.get(Calendar.DAY_OF_WEEK)
-
-        return (hour in 0..6) || day == Calendar.SUNDAY
+) : CoroutineWorker(context, workerParameters) {
+    override suspend fun doWork(): Result {
+        TODO("Not yet implemented")
     }
 }
